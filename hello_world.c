e630bc0d0bc22dcd6eceb415ea47904f3fc290fe
unknown <you@example.com>
2019-11-01 09:46:54 +0900

    branch2ndにいるという文章を付けた

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,8 @@
-My hobby is sports.
 #include<stdio.h>
 
 int main(void){
- printf("hello_world\n");
- return(0);
+	printf("hello_world\n");
+	printf("My hobby is sports.\n");
+	printf("I am in branch2nd.\n");
+	return(0);
 }
